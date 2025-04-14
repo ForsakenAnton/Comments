c592527261a28abc364ff6976de8c6da976a831f
Anton <forsakenanton@gmail.com>
2025-04-14 12:27:32 +0300

    Add CommentTextValidator using HtmlSanitizer NuGet

---
+++ Comments.Server/Models/ValidationModels/CommentTextValidator.cs
@@ -0,0 +1,28 @@
+﻿// Ignore Spelling: Validator
+
+using Ganss.Xss;
+
+namespace Comments.Server.Models.ValidationModels;
+
+public class CommentTextValidator
+{
+    private readonly HtmlSanitizer _sanitizer;
+
+    public CommentTextValidator()
+    {
+        _sanitizer = new HtmlSanitizer();
+        _sanitizer.AllowedTags.Clear();
+        _sanitizer.AllowedTags.Add("a");
+        _sanitizer.AllowedTags.Add("code");
+        _sanitizer.AllowedTags.Add("i");
+        _sanitizer.AllowedTags.Add("strong");
+
+        _sanitizer.AllowedAttributes.Add("href");
+        _sanitizer.AllowedAttributes.Add("title");
+    }
+
+    public string SanitizeText(string input)
+    {
+        return _sanitizer.Sanitize(input);
+    }
+}
